f81459ccd439be844887ad8469b75c8f0dca98a1
David Clausen <davidclausen2051@gmail.com>
2026-01-30 16:44:07 -0600

    feat: get wrangler preview working

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,27 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
-import solidJs from '@astrojs/solid-js';
+import { defineConfig } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: cloudflare(),
-  integrations: [solidJs()]
+	output: "server",
+	adapter: cloudflare({
+		imageService: "compile",
+	}),
+	integrations: [solidJs()],
+	vite: {
+		ssr: {
+			external: [
+				"node:crypto",
+				"node:fs",
+				"node:stream",
+				"node:os",
+				"node:path",
+				"node:util",
+				"node:child_process",
+				"node:events",
+			],
+		},
+	},
 });
